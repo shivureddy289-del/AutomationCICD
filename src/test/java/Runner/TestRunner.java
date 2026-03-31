31605e8c227894500579744e2434294c2686bb88
shivureddy289-del <shivu.reddy289@gmail.com>
2026-03-31 17:45:42 +0530

    updated

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -5,11 +5,21 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-@Suite
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value ="stepdefinitions")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "stepdefinitions",
+    tags = "",
+    plugin = {"pretty", "html:target/cucumber-reports/report.html"}
+)
+//@Suite
+//@SelectClasspathResource("features")
+//@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value ="stepdefinitions")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class TestRunner {
 
 }
