7675b52c11b29590c2145775ab12987ab07d09c8
shivureddy289-del <shivu.reddy289@gmail.com>
2026-04-04 11:15:29 +0530

    updated reports

--- src/main/java/AbstractComponents/ExtentReporterNg.java
+++ src/main/java/AbstractComponents/ExtentReporterNg.java
@@ -1,4 +1,6 @@
 package AbstractComponents;
+import java.io.File;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
@@ -6,7 +8,11 @@ public class ExtentReporterNg {
 
 	public static ExtentReports getReportObject() {
 		
-		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		String path = System.getProperty("user.dir")+"/reports/index.html";
+		
+		// Create reports folder if it doesn't exist
+	    new File(System.getProperty("user.dir") + "/reports").mkdirs();
+		
 		ExtentSparkReporter reporter =new ExtentSparkReporter(path);
 		reporter.config().setReportName("Web Automation Results");
 		reporter.config().setDocumentTitle("Test Results");
